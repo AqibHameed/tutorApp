9bf972a515d75192e1ac8604906fa05fb5cf1a6a
aqibhameed <aqibpadana@gmail.com>
2018-10-11 19:21:21 +0500

    token in header

--- app/controllers/api_controller_controller.rb
+++ app/controllers/api_controller_controller.rb
@@ -2,7 +2,7 @@ class ApiControllerController < ActionController::API
 
   def authenticate_user_from_id_and_token!
 
-    @user = User.find_by_authentication_token(params[:stoken])
+    @user = User.find_by_authentication_token(request.headers[:stoken])
 
     if @user.nil?
       render json: {success: false, message: "Unauthorized User"}, status: 401
--- app/views/users/user_role.json.jbuilder
+++ app/views/users/user_role.json.jbuilder
@@ -1,7 +1,12 @@
 if @tutor.present?
   json.extract! @tutor,  :fees, :education, :experience, :availablity
 elsif @student.present?
-  json.extract! @student,  :price, :timing
+  json.extract! @student,  :price
+  if @student.timing.present?
+    json.timing @student.timing.localtime.strftime("%I:%M %p")
+  else
+    json.timing ""
+  end
 else
 
 end
